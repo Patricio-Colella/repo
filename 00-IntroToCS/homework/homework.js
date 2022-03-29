7a32864992f1904424fc5c06487edcdfecb3d4c4
Parricio <patriciogabriel colella@gmail.com>
2022-03-28 23:32:47 -0300

    finished homework

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,29 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+   var s=num.toString();
+   var bin=0;
+   var x=s.length-1;
+   for(let i=0;i<s.length;i++ ){
+      bin=bin + (s[i] * Math.pow(2,x));
+      x=x-1;
+    }
+   return bin;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var x;
+  var sum=0;
+  var mul=1;
+  do{
+    x=num%2;
+    num=Math.floor(num/2);
+    sum=sum + x*mul;
+    mul=mul*10;
+  }while(num>1)
+  sum=sum + 1*mul;
+  return sum.toString();
 }
 
 
